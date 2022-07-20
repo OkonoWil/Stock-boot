3a6a9185ab608f3f0dc756c41c7f913d0ec0becd
OkonoWil <okonowilfried@gmail.com>
2022-07-21 00:34:54 +0100

    index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,7 +12,7 @@
             <div class=" w-1/6 text-center">ID</div>
             <div class=" w-1/6 text-center">TITLE</div>
             <div class=" w-1/6 text-center">PRICE</div>
-            <div class=" w-1/6 text-center">ACTION</div>
+            <div class=" w-1/2 text-center">ACTION</div>
         </div>
         @if($products->count()==0)
         <div class="flex flex-row justify-center text-xl"><p>No product in stock</p></div>
@@ -22,18 +22,14 @@
             <div class=" w-1/6 border p-3">{{$product['id']}}</div>
             <div class=" w-1/6 border p-3">{{$product['name']}}</div>
             <div class=" w-1/6 border p-3">{{$product['price']}}FCFA</div>
-            <div class=" w-1/6 text-blue-500 border p-3">
-                <a href="{{route('products.details', ['id'=>$product['id']])}}">details</a>
-                <a href="{{route('products.edit', ['id'=>$product['id']])}}">edit</a>
-                <form action="{{route('products.destroy', ['id' =>$product['id']])}}" method="post">
+            <div class=" w-1/6 border p-3">
+                <a class="text-blue-500 " href="{{route('products.details', ['id'=>$product['id']])}}">details</a>
+                <a class="text-green-400" href="{{route('products.edit', ['id'=>$product['id']])}}">edit</a>
+                <form class="text-red-500" action="{{route('products.destroy', ['id' =>$product['id']])}}" method="post">
                     @method('DELETE')
                     @csrf
                     <input type="submit" value="DESTROY">
                 </form>
-            </div>
-            <div class=" w-1/6 text-green-400 border p-3"></div>
-            <div class=" w-1/6 text-red-500 border p-3">
-                
             </div>
         </div>
             
